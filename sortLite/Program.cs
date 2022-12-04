6bec43b9803fa7b75455cf254299036bc4db0468
Petr <sppetrak@mail.ru>
2022-12-04 21:27:47 +0300

    сортировка библиотекой

---
+++ sortLite/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array  = {3, 5, 8, 9, 5, 7 ,1, 3, 6, 2};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+}
+PrintArray(array);
+Array.Sort(array);
+
+PrintArray(array);
