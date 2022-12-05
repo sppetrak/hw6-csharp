ea1d90294644e45b537ec1ad5c0f651dfad60846
Petr <sppetrak@mail.ru>
2022-12-05 20:34:12 +0300

    исправлен метод сортировки

--- sortnotworked/Program.cs
+++ sortnotworked/Program.cs
@@ -2,19 +2,30 @@
 // количества элементов (не менее 6)в промежутке от 0 до 100, а на выходе выводит этот же массив, 
 // но отсортированный по возрастанию(от меньшего числа к большему).
 
-
-int[] array = {6, 3, 9, 8, 1, 5, 6, 2, 4, 7};
-
+Console.WriteLine("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine()!);
+int[] array = FillArray(size);
 PrintArray(array);
+
+int[] FillArray(int size,int  minVal = 0,int  maxVal = 100)
+{
+    int[] array =new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(0,100);
+    }
+    return array;
+}
+
 
 void PrintArray(int[] array)
 {
     int count = array.Length;
-for (int i = 0; i < count; i++)
+    for (int i = 0; i < count; i++)
     {
         Console.Write($"{array[i]} ");
-}
-Console.WriteLine();
+    }
+    Console.WriteLine();
 }
 
 
@@ -25,14 +36,15 @@ void SortedArray(int[] array)
         int minPosition = i;
         for (int j = i + 1; j < array.Length; j++)
         {
-            if (array[j] < array[j]) minPosition = j;
+            if (array[j] < array[i])
             {
                 minPosition = j;
+                int temp = array[i];
+                array[i] = array[minPosition];
+                array[minPosition] = temp;
             }
         }
-        int temp = array[i];
-        array[i] = array[minPosition];
-        array[minPosition] = temp;
+
     }
 }
 SortedArray(array);
